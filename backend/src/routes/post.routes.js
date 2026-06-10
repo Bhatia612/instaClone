19f967fde1198f9e6a6529badc1b9384a3dbfc10
MOHIT BHATIA <mohitbhatia612@gmail.com>
2026-06-10 01:02:34 -0400

    fix: moved authenticate before upload and protected get-posts route

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,15 +1,14 @@
 const express = require("express")
 const postControllers = require("../controllers/post.controllers")
-const identifyuser = require("../middlewares/auth.middleware")
-
 const multer = require("multer")
+const authenticate = require("../middlewares/auth.middleware")
 const upload = multer({ storage: multer.memoryStorage() })
 
 const postRouter = express.Router()
 
 
-postRouter.get("/get-posts", postControllers.getPostsController)
-postRouter.post("/create-new-post", upload.single("image"), identifyuser, postControllers.createPostController)
-postRouter.post("/like/:postId", identifyuser, postControllers.likePostController)
+postRouter.get("/get-posts", authenticate, postControllers.getPostsController)
+postRouter.post("/create-new-post", authenticate, upload.single("image"), postControllers.createPostController)
+postRouter.post("/like/:postId", authenticate, postControllers.likePostController)
 
 module.exports = postRouter
