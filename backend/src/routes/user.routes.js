42e0cae41cdedca678e69cf0ed69a80310202763
MOHIT BHATIA <mohitbhatia612@gmail.com>
2026-06-10 00:59:56 -0400

    fix: protected fetch-users route and updated middleware name

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,15 +1,15 @@
 const express = require("express")
 const userControllers = require("../controllers/user.controllers")
-const identifyuser = require("../middlewares/auth.middleware")
+const authenticate = require("../middlewares/auth.middleware")
 
 const userRouter = express.Router()
 
-userRouter.get("/fetch-users", userControllers.getUserController)
-userRouter.post("/:followeeId/follow", identifyuser, userControllers.followUserController)
-userRouter.post("/:followeeId/unfollow", identifyuser, userControllers.unFollowUserController)
-userRouter.get("/follow-requests/fetch", identifyuser, userControllers.fetchFollowRequestsController)
-userRouter.post("/follow-req/:reqId/accept", identifyuser, userControllers.acceptFollowRequest)
-userRouter.post("/follow-req/:reqId/reject", identifyuser, userControllers.rejectFollowRequest)
+userRouter.get("/fetch-users", authenticate, userControllers.getUserController)
+userRouter.post("/:followeeId/follow", authenticate, userControllers.followUserController)
+userRouter.post("/:followeeId/unfollow", authenticate, userControllers.unFollowUserController)
+userRouter.get("/follow-requests/fetch", authenticate, userControllers.fetchFollowRequestsController)
+userRouter.post("/follow-req/:reqId/accept", authenticate, userControllers.acceptFollowRequest)
+userRouter.post("/follow-req/:reqId/reject", authenticate, userControllers.rejectFollowRequest)
 
 
 
