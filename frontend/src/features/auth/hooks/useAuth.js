44eaa16937953f0170306d93152da4a8adf967a4
MOHIT BHATIA <mohitbhatia612@gmail.com>
2026-06-12 16:14:44 -0400

    feat: added handleLogout and null check to useAuth hook

--- frontend/src/features/auth/hooks/useAuth.js
+++ frontend/src/features/auth/hooks/useAuth.js
@@ -6,6 +6,10 @@ import { register, login } from "../services/auth.api";
 export const useAuth = () => {
     const context = useContext(AuthContext)
 
+    if (!context) {
+        throw new Error("useAuth must be used within an AuthProvider")
+    }
+
     const { user, setUser, loading, setLoading } = context
 
     const handleRegister = async (username, email, password) => {
@@ -14,9 +18,6 @@ export const useAuth = () => {
         try {
             const response = await register(username, email, password)
             setUser(response.user)
-        }
-        catch (error) {
-            throw error
         } finally {
             setLoading(false)
         }
@@ -29,15 +30,22 @@ export const useAuth = () => {
         try {
             const response = await login(usernameOrEmail, password)
             setUser(response.user)
+
+        } finally {
+            setLoading(false)
         }
-        catch (error) {
-            throw error
+    }
+    const handleLogout = async () => {
+        setLoading(true)
+        try {
+            await logout()
+            setUser(null)
         } finally {
             setLoading(false)
         }
     }
 
     return {
-        user, loading, handleRegister, handleLogin
+        user, loading, handleRegister, handleLogin, handleLogout
     }
 }
