0fbe198e16ab05fe6af75e2d4f6b14ed86121c46
MOHIT BHATIA <mohitbhatia612@gmail.com>
2026-06-12 16:46:24 -0400

    fix: used env variable for base URL, and minor fixex

--- frontend/src/features/posts/services/posts.api.js
+++ frontend/src/features/posts/services/posts.api.js
@@ -1,24 +1,23 @@
-import axios, { formToJSON } from "axios"
+import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/posts",
+    baseURL: `${import.meta.env.VITE_API_URL}/posts`,
     withCredentials: true
 })
 
 export async function getPostsForFeed() {
     const response = await api.get("/get-posts")
-    console.log(response.data)
     return response.data
 
 }
 
 export async function createPost(postImageFile, caption) {
-    const fromDate = new FormData()
+    const formDate = new FormData()
 
-    fromDate.append("caption", caption)
-    fromDate.append("image", postImageFile)
-    const response = await api.post("/create-new-post", fromDate)
+    formDate.append("caption", caption)
+    formDate.append("image", postImageFile)
+    const response = await api.post("/create-new-post", formDate)
 
     return response.data
 }
